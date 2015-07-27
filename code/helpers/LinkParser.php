9880eb973f71594af5a87d37ce86df07d81b6c0a
Daphne Wang <Daphne.Wang@dia.govt.nz>
2015-07-27 17:17:18 +1200

    New tests, changed regex to ignore case.

--- code/helpers/LinkParser.php
+++ code/helpers/LinkParser.php
@@ -4,7 +4,7 @@ class LinkParser {
 
     public static function find_links($string) {
 
-        $pattern = '/\b(?:http:|https:)(?:[^\s,."!?]|[,.!?](?!\s))+/';
+        $pattern = '/\b(?:http:|https:)(?:[^\s,."!?]|[,.!?](?!\s))+/i'; //TODO: make http/https non-compulsory, and factor in things such as parameters following the main domain
         preg_match_all($pattern, $string, $matches);
 
         return $matches[0];
--- tests/LinkParserTest.php
+++ tests/LinkParserTest.php
@@ -26,4 +26,129 @@ class LinkParserTest extends SapphireTest {
         $this->assertEquals(1, count($actual));
 
     }
+
+    public function testLinkInAnchorTagsDoubleQ() {
+        $hasString = '<a href="http://www.222.com">Resources and Guides</a>';
+        $expected = 'http://www.222.com';
+        $actual = LinkParser::find_links($hasString);
+
+        $this->assertContains($expected, $actual, 'Actual link did not match expected.');
+
+        $this->assertEquals(1, count($actual));
+    }
+
+    //Valid html can wrap links with single quotes, and this test fails to pass.
+    public function testLinkInAnchorTagsSingleQ() {
+        $hasString = "<a href='http://www.222.com'>Resources and Guides</a>";
+        $expected = 'http://www.222.com';
+
+        $actual = LinkParser::find_links($hasString);
+        Debug::show($actual);
+        $this->assertContains($expected, $actual, 'Actual link did not match expected.');
+
+        $this->assertEquals(1, count($actual));
+    }
+
+    public function testHttpsLink() {
+        $hasString = 'https://www.222.com';
+        $expected = 'https://www.222.com';
+        $actual = LinkParser::find_links($hasString);
+
+        $this->assertContains($expected, $actual, 'Actual link did not match expected.');
+
+        $this->assertEquals(1, count($actual));
+    }
+
+    //links in caps should still be recognised
+    public function testCaps() {
+        $hasString = 'HTTP://WWW.222.COM';
+        $expected = 'HTTP://WWW.222.COM';
+        $actual = LinkParser::find_links($hasString);
+        Debug::show($actual);
+        $this->assertContains($expected, $actual, 'Actual link did not match expected.');
+
+        $this->assertEquals(1, count($actual));
+    }
+
+    public function testBackwardsBracket() {
+        $hasString = 'https:\\www.222.com';
+        $expected = 'http://www.222.com';
+        $actual = LinkParser::find_links($hasString);
+
+        $this->assertNotContains($expected, $actual, 'Actual link did not match expected.');
+
+        $this->assertEquals(1, count($actual));
+    }
+
+    public function testInvalidLinkSymbols() {
+        $hasString = 'https://@www.222.com!';
+        $expected = 'http://www.222.com';
+        $actual = LinkParser::find_links($hasString);
+
+        $this->assertNotContains($expected, $actual, 'Actual link did not match expected.');
+
+        $this->assertEquals(1, count($actual));
+    }
+
+    public function testLengthyDomain() {
+
+        $hasString = 'http://www.llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogochuchaf.com';
+        $expected = 'http://www.llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogochuchaf.com';
+        $actual = LinkParser::find_links($hasString);
+
+        $this->assertContains($expected, $actual, 'Actual link did not match expected.');
+
+        $this->assertEquals(1, count($actual));
+
+    }
+
+    public function testNet() {
+
+        $hasString = 'http://www.osimerithrerg3.net';
+        $expected = 'http://www.osimerithrerg3.net';
+        $actual = LinkParser::find_links($hasString);
+
+        $this->assertContains($expected, $actual, 'Actual link did not match expected.');
+
+        $this->assertEquals(1, count($actual));
+
+    }
+
+    public function testOrg() {
+
+        $hasString = 'http://www.osimerithrerg3.org';
+        $expected = 'http://www.osimerithrerg3.org';
+        $actual = LinkParser::find_links($hasString);
+
+        $this->assertContains($expected, $actual, 'Actual link did not match expected.');
+
+        $this->assertEquals(1, count($actual));
+
+    }
+
+    public function testInfo() {
+
+        $hasString = 'http://www.osimerithrerg3.info';
+        $expected = 'http://www.osimerithrerg3.info';
+        $actual = LinkParser::find_links($hasString);
+
+        $this->assertContains($expected, $actual, 'Actual link did not match expected.');
+
+        $this->assertEquals(1, count($actual));
+
+    }
+
+    public function testCoNz() {
+
+        $hasString = 'http://www.osimerithrerg3.co.nz';
+        $expected = 'http://www.osimerithrerg3.co.nz';
+        $actual = LinkParser::find_links($hasString);
+
+        $this->assertContains($expected, $actual, 'Actual link did not match expected.');
+
+        $this->assertEquals(1, count($actual));
+
+    }
+
+
 }
